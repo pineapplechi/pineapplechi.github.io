dc6d62ec7e1c397d74ce6e9006133ab302851923
pineapple <alexandersmithlife@gmail.com>
2025-01-18 13:03:12 -0500

    New Wave

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
-from flask import Flask, render_template, request
+# app.py
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, BlogPost
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SECRET_KEY'] = 'your_secret_key'
 
-@app.route("/")
-def index():
-    return "Hello, World!!"
+db.init_app(app)
+
+@app.route('/')
+def home():
+    posts = BlogPost.query.order_by(BlogPost.date_posted.desc()).all()
+    return render_template('index.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/new', methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        new_post = BlogPost(title=title, content=content)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('new_post.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
